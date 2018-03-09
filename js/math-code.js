02f93423eb07fed2f421a37859830b3198c9e9c5
uvraag <uvraag@outlook.com>
2018-03-08 19:03:25 -0800

    Add space, update math-code.js

--- js/math-code.js
+++ js/math-code.js
@@ -20,6 +20,33 @@ function mcAddSection( first, second ){
 		first.shift();
 	while( Number( second[0] )== 0 )
 		second.shift();
+	if( first[0] == "-"  && second[0] == "-" ) {
+		first = first.slice( 1 );
+		second = second.slice( 1 );
+		first = first.join( "" );
+		second = second.join( "" );
+		third = mcAddSection( first, second );
+		third = third.split( "" );
+		third.unshift( "-" );
+		third = third.join( "" );
+		return third;
+	} else if ( first[0] != "-" && second[0] == "-" ) {
+		second = second.slice( 1 );
+		first = first.join( "" );
+		second = second.join( "" );
+		third = mcSubSection( first, second );
+		third = third.split( "" );
+		third = third.join( "" );
+		return third;
+	} else if ( first[0] == "-" && second[0] != "-" ) {
+		first = first.slice( 1 );
+		first = first.join( "" );
+		second = second.join( "" );
+		third = mcSubSection( second, first );
+		third = third.split( "" );
+		third = third.join( "" );
+		return third;
+	}
 	for( i = 0; i < first.length; i++ )
 		if( first[i] == "." ){
 			decimalfirst = true;
@@ -32,20 +59,20 @@ function mcAddSection( first, second ){
 			secondpos = i;
 			break;
 		}
-	if( decimalfirst == false && decimalsecond == false ){
+	if( decimalfirst == false && decimalsecond == false ) {
 		if( first.length > second.length )
 			lengthno = first.length;
 		else
 			lengthno = second.length;
 		firstLastIndex = first.length - 1;
 		secondLastIndex = second.length - 1;
-		for( i = 0; i < lengthno; i++, firstLastIndex--, secondLastIndex--){
+		for( i = 0; i < lengthno; i++, firstLastIndex--, secondLastIndex--) {
 			firstnohold = (firstLastIndex < 0)? 0 : Number( first[ firstLastIndex ] );
 			secondnohold = (secondLastIndex < 0)? 0 : Number( second[ secondLastIndex ] );
 			third[i] = (isNaN( third[i] ))? 0 : third[i];
 			third[i] = Number( third[i] ) + firstnohold + secondnohold;
 			third[i] = String( third[i] );
-			if( third[i].length == 2 ){
+			if( third[i].length == 2 ) {
 				third[ i + 1 ] = third[i].charAt( 0 );
 				third[ i ] = third[i].charAt( 1 );
 			}
@@ -54,15 +81,15 @@ function mcAddSection( first, second ){
 		third = third.join( "" );
 		return third;
 	}
-	else if( decimalfirst == true || decimalsecond == true ){
+	else if( decimalfirst == true || decimalsecond == true ) {
 
-		if( decimalfirst == false ){
+		if( decimalfirst == false ) {
 			first = first.join( "" );
 			firstpos = first.length;
 			first = first.concat( ".0" );
 			first = first.split( "" );
 		}
-		else if( decimalsecond == false ){
+		else if( decimalsecond == false ) {
 			second = second.join( "" );
 			secondpos = second.length;
 			second = second.concat( ".0" );
@@ -87,10 +114,10 @@ function mcAddSection( first, second ){
 		thirdslice = mcAddSection( firstslice, secondslice );
 
 		if( firstslice.length == secondslice.length &&
-			thirdslice.length == firstslice.length ){
+			thirdslice.length == firstslice.length ) {
 
 			thirdslice = thirdslice.split( "" );
-			while( thirdslice[ thirdslice.length - 1 ] == 0 )
+			while( thirdslice[thirdslice.length - 1] == 0 )
 				thirdslice.pop();
 			thirdslice = thirdslice.join( "" );
 			third = thirdhalf.concat( ".", thirdslice );
@@ -102,7 +129,7 @@ function mcAddSection( first, second ){
 			thirdhalf = mcAddSection( thirdhalf, thirdslice.charAt( 0 ) );
 			thirdslice = thirdslice.split( "" );
 			thirdslice.shift();
-			while( thirdslice[ thirdslice.length - 1 ] == 0 )
+			while( thirdslice[thirdslice.length - 1] == 0 )
 				thirdslice.pop();
 			thirdslice = thirdslice.join( "" );
 
@@ -112,13 +139,15 @@ function mcAddSection( first, second ){
 	}
 	return "Something went wrong";
 }
-function mcAdd(){
+
+function mcAdd() {
 	var third = 0;
-	for( var i = 0, j = arguments.length; i < j; i++ ){
-		third = mcAddSection( arguments[ i ], third );
+	for( var i = 0, j = arguments.length; i < j; i++ ) {
+		third = mcAddSection( arguments[i], third );
 	}
 	return third;
 }
+
 function mcMulOne( number, one ){
 	var lengthno,
 		i,
